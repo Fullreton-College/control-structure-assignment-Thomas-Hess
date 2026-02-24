1b80174b4eeb6e25927f826f745f64060eb95dda
Thomas-Hess <2degreesummit@gmail.com>
2026-02-23 18:12:52 -0800

    Update SoftwareStore.cpp
    Created by Thomas Hess

--- SoftwareStore.cpp
+++ SoftwareStore.cpp
@@ -1,16 +1,42 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-int main()
-{
-    double mph = 6.5;
-    // PACE calculation
-    /* write your code */
 
-    //express pace in minutes and seconds
-  /* write your code here */
+int main() {
+    // define variables
+    int units;
+    double discount;
 
-  //express your output in 1 decimal points
+    // ask user for number of units
+    cout << "Software package selling for $199! How many units do you want: ";
+    cin >> units;
 
+    // calculate discount
+    if (units >= 10 && units <= 19) {
+        discount = 20;
+    }
+    else if (units >= 20 && units <= 49) {
+        discount = 30;
+    }
+    else if (units >= 50 && units <= 99) {
+        discount = 40;
+    }
+    else if (units >= 100) {
+        discount = 50;
+    }
+    else {
+        discount = 0;
+    }
 
+    double totalPrice = 199 * units * (1 - (discount / 100));
+
+    // output all relevant info
+    cout << "You are eligible for a " << discount << " percent discount.\n";
+    // have price rounded to cents
+    cout << fixed << setprecision(2);
+    cout << "Your total price for the " << units << " units comes to $"
+         << totalPrice << ".\n";
+
+    // end program
+    return 0;
 }
